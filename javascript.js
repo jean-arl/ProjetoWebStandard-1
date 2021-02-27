eca823cb359d8ffdd875cfabd1a278e514231d93
Jean <65451480+jean-arl@users.noreply.github.com>
2021-02-27 14:01:29 -0300

    modelando a vitrine

--- javascript.js
+++ javascript.js
@@ -6,5 +6,11 @@ $(function(){
         slidesToScroll: 1,
         centerMode:true,
     });
-    
+
+    $(".vitrine-slider").on('afterChange', function(){
+        $("#camiseta-name").text($(".slick-center").data("name"));
+        var camisetaPrice = parseFloat($(".slick-center").data("price")).toLocaleStringt("pt-BR", {style:"currency", currency:"BRL", minimumFractionDigits: 2});
+        $("#camiseta-price").text(camisetaPrice);
+
+    });
 })
